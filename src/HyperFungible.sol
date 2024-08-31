4e6d33e48250f72ec78a66cd5dd9851faecaf5ba
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-08-31 16:09:33 +0200

    some fixes

--- src/HyperFungible.sol
+++ src/HyperFungible.sol
@@ -3,10 +3,11 @@ pragma solidity 0.8.26;
 
 /* Chainlink Libraries */
 import {Client} from "@chainlink/contracts/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts/ccip/applications/CCIPReceiver.sol";
 
 /* Chainlink Interfaces */
 import {IRouterClient} from "@chainlink/contracts/ccip/interfaces/IRouterClient.sol";
-import {LinkTokenInterface} from "@chainlink/contracts/";
+//import {LinkTokenInterface} from "@chainlink/contracts/";
 
 /* OpenZeppelin Interfaces */
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -14,12 +15,13 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /* HyperFungible Libraries */
 import {HFDataTypes} from "src/libraries/HFDataTypes.sol";
 
-contract HyperFungible {
+contract HyperFungible is CCIPReceiver {
 
     HFDataTypes.CcipFeesIn public ccipFeesIn;
-    address ccipRouter;
-    address link;
-    uint32 chainId;
+    address public ccipRouter;
+    address public link;
+    uint32 public chainId;
+    mapping(uint64 destinations => address receiver) public receiverOnChain;
 
     constructor (address _ccipRouter, address _link, uint32 _chainId) {
         ccipFeesIn = HFDataTypes.CcipFeesIn.NATIVE;
@@ -28,7 +30,7 @@ contract HyperFungible {
         chainId = _chainId;
     }
 
-    function hyperfyTokens(HFDataTypes.HyperfyOrder _order, uint64 _destination, address _receiver) external returns(bytes32 _messageId) {
+    function hyperfyTokens(HFDataTypes.HyperfyOrder _order, uint64 _destination) external returns(bytes32 _messageId) {
         IERC20(_order.token).transferFrom(msg.sender, address(this), _order.amount);
 
         IRouterClient router = IRouterClient(ccipRouter);
@@ -39,7 +41,7 @@ contract HyperFungible {
         });
 
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(_receiver),
+            receiver: abi.encode(receiverOnChain[_destination]),
             data: abi.encode(fullOrder),
             tokenAmounts: new Client.EVMTokenAmount[](0),
             extraArgs: "",
